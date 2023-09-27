6ee1c98c6fdebba50d36765123990ddce1abf1a6
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-09-27 17:46:53 +0200

    V0.1

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+  
+const hamburgerIcon = document.querySelector('.hamburger-icon-mob');
+const menuItems = document.querySelector('.menu-items-mob');
+hamburgerIcon.addEventListener('click', () => {
+hamburgerIcon.classList.toggle('active');
+menuItems.classList.toggle('active');
+});
