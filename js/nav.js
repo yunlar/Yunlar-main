0f10cbec96ea85992cafa3192ebc00bc2ffb41ad
Guus Kaashoek <121664727+guuskaashoek@users.noreply.github.com>
2023-12-12 19:35:18 +0100

    Delete js directory

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-const hamburgerIcon = document.querySelector('.hamburger-icon-mob');
-const menuItems = document.querySelector('.menu-items-mob');
-hamburgerIcon.addEventListener('click', () => {
-hamburgerIcon.classList.toggle('active');
-menuItems.classList.toggle('active');
-});
