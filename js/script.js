cde3d63ddf47f3d8c6b6a0ecee02985ae27e4900
Guus Kaashoek <Guus.kaashoek@gmail.com>
2023-12-19 20:15:27 +0100

    Update mobile menu and logo

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@ if (/Mobi/.test(navigator.userAgent) && !/iPad/.test(navigator.userAgent)) {
   const menuElement = document.querySelector('.menu-items');
   const iconElement = document.querySelector('.hamburger-icon');
   const logoElement = document.querySelector('.logo');
+  
 
   menuElement.classList.remove('menu-items');
   menuElement.classList.add('menu-items-mob');
 
   iconElement.classList.remove('hamburger-icon');
   iconElement.classList.add('hamburger-icon-mob');
-
+  
+  logoElement.src = "assets/logo/icon-white.svg";
   logoElement.classList.remove('logo');
   logoElement.classList.add('logo-mob');
 
-  logoElement.src = "assets/logo/icon-white.svg";
+  
 }
 
   const hamburgerIcon = document.querySelector('.hamburger-icon-mob');
